b0c2bd85268181bb5e4f34a388ce5d67e678c871
Zeyad Amr <zeyad.amr262000@gmail.com>
2021-05-21 21:59:17 +0200

    init

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,27 +1,10 @@
 
 package bank;
 
-/* 
-//import java.util.Scanner;
 public class Bank {
 
     public static void main(String[] args) {
 
     }
 
-}
- */
-import javax.swing.*;
-
-class gui {
-    public static void main(String args[]) {
-        JFrame frame = new JFrame("My First GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        JButton button1 = new JButton("Button 1");
-        JButton button2 = new JButton("Button 2");
-        frame.getContentPane().add(button1);
-        frame.getContentPane().add(button2);
-        frame.setVisible(true);
-    }
 }
