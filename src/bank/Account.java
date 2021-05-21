0fc0db9abd9a201a5678c41c40401d37e2286119
Zeyad Amr <zeyad.amr262000@gmail.com>
2021-05-21 23:51:06 +0200

    accountClassInit

---
+++ src/bank/Account.java
@@ -0,0 +1,14 @@
+package bank;
+
+public class Account {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamee() {
+        return name;
+    }
+}
--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,10 +1,18 @@
 
 package bank;
 
+import java.util.Scanner;
+
 public class Bank {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        Account user = new Account();
+        user.setName(input.nextLine());
+        System.out.println(user.getNamee());
+        input.close();
     }
 
 }
