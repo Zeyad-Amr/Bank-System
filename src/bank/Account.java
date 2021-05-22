5fbebab8c6dd8d0b7736c67afcad5e5db6f270bc
omaranwar21 <omarahmed11185@gmail.com>
2021-05-23 00:45:07 +0200

    333

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -3,12 +3,67 @@ package bank;
 public class Account {
 
     private String name;
+    private String nationalId;
+    private String password;
+    private String phone;
+    private Float balance;
+    private String birthDate;
+    private Float creditBalance;
+    private Boolean status;
+    private String creditDate;
+    private String creditEndDate;
+    
+    
 
-    public void setName(String name) {
+        public void setName(String name) {
         this.name = name;
+            }
+        public String getName() {
+        return name;
+            }
+
+
+        public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+            }
+        public String getNationalId() {
+        return nationalId;
+            }
+
+
+        public void setPassword(String password) {
+        this.password = password;
+            }
+        public String getPassword() {
+        return password;
+            }
+
+            
+        public void setPhone(String phone) {
+        this.phone = phone;
+            }
+        public String getPhone() {
+        return phone;
+            }
+
+
+        public void setBalance(Float balance){
+        this. balance = balance ;
+            }
+        public Float getBalance(){
+        return balance;
+            } 
+
+        public void setBirthDate(String birthDate){
+        this.birthDate = birthDate;
+            }
+        public String getBirthDate(){
+        return birthDate;
+            }
+        
+
+        
     }
 
-    public String getNamee() {
-        return name;
-    }
-}
+    
+ // git config --global user.name "Your Name"
--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -6,13 +6,32 @@ import java.util.Scanner;
 public class Bank {
 
     public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
+Scanner input = new Scanner(System.in);
+    
+        System.out.println("enter your name");
+        
         Account user = new Account();
         user.setName(input.nextLine());
-        System.out.println(user.getNamee());
+        System.out.println(user.getName());
+
+    System.out.println("enter your ID");
+ 
+        user.setNationalId(input.nextLine());
+        System.out.println(user.getNationalId());
         input.close();
+        
+    System.out.println("enter your password");
+ 
+        user.setPassword(input.nextLine());
+        System.out.println(user.getPassword());
+        input.close();
+
+    System.out.println("enter your phone");
+        
+        user.setPhone(input.nextLine());
+        System.out.println(user.getPhone());
+        input.close();
+
     }
 
 }
